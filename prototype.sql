63cc014ba20b3140e75cc217e7c695fa6252d67f
JOSEALM3IDA <josealmeida.dev@gmail.com>
2022-08-05 03:07:31 +0100

    More prototype changes

--- prototype.sql
+++ prototype.sql
@@ -8,10 +8,10 @@
 
 -- Period 1: 2019
 -- Period 2: 2020
-DECLARE start_p1 DATE DEFAULT DATE("2019-01-01");
-DECLARE end_p1 DATE DEFAULT DATE("2019-12-31");
-DECLARE start_p2 DATE DEFAULT DATE("2020-01-01");
-DECLARE end_p2 DATE DEFAULT DATE("2020-12-31");
+DECLARE d_start_p1 DATE DEFAULT DATE("2019-01-01");
+DECLARE d_end_p1 DATE DEFAULT DATE("2019-12-31");
+DECLARE d_start_p2 DATE DEFAULT DATE("2020-01-01");
+DECLARE d_end_p2 DATE DEFAULT DATE("2020-12-31");
 
 CREATE OR REPLACE PROCEDURE cp_gain_loss_bachelor_thesis.calc_gain_loss_rp_fmcg_prototype(
   start_p1 DATE, end_p1 DATE, start_p2 DATE, end_p2 DATE)
@@ -24,97 +24,97 @@ table_hh_brand_period_rp AS (
     These different values per period are promptly available when they need to be used together.
     In this first step, the values are not yet transposed, but are split in different columns, writing zeros when the values don't refer to that specific period.
     These columns can later be summed together, obtaining the desired table. */
-    SELECT hhkey, marke,
-        CASE WHEN period = 1 THEN SUM(wert * markenfactor) ELSE 0 END AS spent_p1_ics, -- Intermediate Continuous Sample value, Period 1
-        CASE WHEN period = 2 THEN SUM(wert * markenfactor) ELSE 0 END AS spent_p2_ics, -- Intermediate Continuous Sample value, Period 2
-        CASE WHEN period = 1 THEN SUM(wert * markenfactor * rwkorr * rwstd * fullmasW) ELSE 0 END AS spent_p1_fs, -- Full Sample value, Period 1
-        CASE WHEN period = 2 THEN SUM(wert * markenfactor * rwkorr * rwstd * fullmasW) ELSE 0 END AS spent_p2_fs, -- Full Sample value, Period 2
+    SELECT hhkey, brand,
+        CASE WHEN period = 1 THEN SUM(value * brandfactor) ELSE 0 END AS spent_p1_ics, -- Intermediate Continuous Sample value, Period 1
+        CASE WHEN period = 2 THEN SUM(value * brandfactor) ELSE 0 END AS spent_p2_ics, -- Intermediate Continuous Sample value, Period 2
+        CASE WHEN period = 1 THEN SUM(value * brandfactor * rwkorr * rwstd * fullmasW) ELSE 0 END AS spent_p1_fs, -- Full Sample value, Period 1
+        CASE WHEN period = 2 THEN SUM(value * brandfactor * rwkorr * rwstd * fullmasW) ELSE 0 END AS spent_p2_fs, -- Full Sample value, Period 2
     FROM (
         -- Filtering values according to date, deciding if each value belongs to the 1st or 2nd period
-        SELECT hhkey, marke, wert, markenfactor, rwkorr, rwstd, fullmasW,
-            CASE WHEN datum >= start_p1 AND datum <= end_p1 THEN 1
-            ELSE CASE WHEN datum >= start_p2 AND datum <= end_p2 THEN 2
+        SELECT hhkey, brand, value, brandfactor, rwkorr, rwstd, fullmasW,
+            CASE WHEN datep >= start_p1 AND datep <= end_p1 THEN 1
+            ELSE CASE WHEN datep >= start_p2 AND datep <= end_p2 THEN 2
             ELSE 3 END END AS period
         FROM (
             -- Directly reading necessary columns from the raw data, applying brand aggregation applying brand aggregation and selection (which are parameterized in another table)
             SELECT hhkey,
-                CASE WHEN ba.brand IS NOT NULL THEN ba.aggregation ELSE 'REST' END AS marke,
-                wert, markenfactor, rwkorr, rwstd, fullmasW, PARSE_DATE("%Y%m%d", datum) AS datum
-            FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_fmcg_2019_2020`
+                CASE WHEN ba.brand IS NOT NULL THEN ba.aggregation ELSE 'REST' END AS brand,
+                value, brandfactor, rwkorr, rwstd, fullmasW, PARSE_DATE("%Y%m%d", datep) AS datep
+            FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_fmcg_2019_2020` rawfmcg
             LEFT OUTER JOIN `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.brand_aggregation_fmcg` ba
-            ON marke = ba.brand
+            ON rawfmcg.brand = ba.brand
         )
         
     )
-    GROUP BY hhkey, marke, period
+    GROUP BY hhkey, brand, period
 ),
 table_hh_period_rp AS (
     -- Applying the continuous weights on the intermediate continuous values, obtaining the continuous sample values per household and brand combinations
-    SELECT sumhh.hhkey, marke,
+    SELECT sumhh.hhkey, brand,
         sumhh.spent_p1_ics * rawhh.continuo AS spent_p1_cs,
         sumhh.spent_p2_ics * rawhh.continuo AS spent_p2_cs,
         spent_p1_fs, spent_p2_fs
     FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_households_2019_2020` rawhh, (
         -- Obtain the weights grouped by household, brand and period (equivalent to table 2.2, but compressed/transposed like above explained)
-        SELECT hhkey, marke,
+        SELECT hhkey, brand,
             SUM(spent_p1_ics) AS spent_p1_ics, SUM(spent_p2_ics) AS spent_p2_ics,
             SUM(spent_p1_fs) AS spent_p1_fs, SUM(spent_p2_fs) AS spent_p2_fs
         FROM table_hh_brand_period_rp
-        GROUP BY hhkey, marke
+        GROUP BY hhkey, brand
     ) sumhh
     WHERE sumhh.hhkey = rawhh.hhkey
 ),
 table_pre_run AS (
     -- Calculating the correction factor per brand and period (table 2.3)
-    SELECT marke,
+    SELECT brand,
         IFNULL(SAFE_DIVIDE(brand_sales_p1_fs, brand_sales_p1_cs), 0) AS corr_factor_p1,
         IFNULL(SAFE_DIVIDE(brand_sales_p2_fs, brand_sales_p2_cs), 0) AS corr_factor_p2
     FROM (
         -- Removing household grouping in preparation for the final per-brand calculation
-        SELECT marke,
+        SELECT brand,
             SUM(spent_p1_cs) AS brand_sales_p1_cs, SUM(spent_p2_cs) AS brand_sales_p2_cs,
             SUM(spent_p1_fs) AS brand_sales_p1_fs, SUM(spent_p2_fs) AS brand_sales_p2_fs
         FROM table_hh_period_rp
-        GROUP BY marke
+        GROUP BY brand
     )
 ),
 
--- Pre run
+-- Pre-run
 --------------------------
--- Main run
+-- Main-run
 
 table_hh_brand_period AS (
-    -- Applying the correction factor from the pre run on the main run, upgrading it to the RP run
-    SELECT hhkey, trd.marke,
-        CASE WHEN period = 1 THEN SUM(wert * markenfactor * corr_factor_p1) ELSE 0 END AS spent_p1,
-        CASE WHEN period = 2 THEN SUM(wert * markenfactor * corr_factor_p2) ELSE 0 END AS spent_p2
+    -- Applying the correction factor from the pre-run on the main-run, upgrading it to the RP run
+    SELECT hhkey, trd.brand,
+        CASE WHEN period = 1 THEN SUM(value * brandfactor * corr_factor_p1) ELSE 0 END AS spent_p1,
+        CASE WHEN period = 2 THEN SUM(value * brandfactor * corr_factor_p2) ELSE 0 END AS spent_p2
     FROM (
         -- Separate values according to date, deciding if each value belongs to the 1st or 2nd period (again)
-        SELECT hhkey, marke, wert, markenfactor, rwkorr, rwstd, fullmasW,
-            CASE WHEN datum >= start_p1 AND datum <= end_p1 THEN 1
-            ELSE CASE WHEN datum >= start_p2 AND datum <= end_p2 THEN 2
+        SELECT hhkey, brand, value, brandfactor, rwkorr, rwstd, fullmasW,
+            CASE WHEN datep >= start_p1 AND datep <= end_p1 THEN 1
+            ELSE CASE WHEN datep >= start_p2 AND datep <= end_p2 THEN 2
             ELSE 3 END END AS period
         FROM (
             -- Directly reading necessary columns from the raw data (again), applying brand aggregation and selection (which are parameterized in another table)
             SELECT hhkey,
-                CASE WHEN ba.brand IS NOT NULL THEN ba.aggregation ELSE 'REST' END AS marke,
-                wert, markenfactor, rwkorr, rwstd, fullmasW, PARSE_DATE("%Y%m%d", datum) AS datum
-            FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_fmcg_2019_2020`
+                CASE WHEN ba.brand IS NOT NULL THEN ba.aggregation ELSE 'REST' END AS brand,
+                value, brandfactor, rwkorr, rwstd, fullmasW, PARSE_DATE("%Y%m%d", datep) AS datep
+            FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_fmcg_2019_2020` rawfcmg
             LEFT OUTER JOIN `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.brand_aggregation_fmcg` ba
-            ON marke = ba.brand
+            ON rawfcmg.brand = ba.brand
         )
     ) trd
-    JOIN table_pre_run trp ON trd.marke = trp.marke
-    GROUP BY hhkey, marke, period
+    JOIN table_pre_run trp ON trd.brand = trp.brand
+    GROUP BY hhkey, brand, period
 ),
 table_hh_period AS (
     -- Applying the continuous sample weights (table 2.4). This is done separately to make the query easier to understand.
     -- Theoretically, the weights should be applied at the same time as the corr_factor, but in practice it doesn't matter, due to the distributive property of multiplication
-    SELECT sumhh.hhkey, marke, sumhh.spent_p1 AS spent_p1, sumhh.spent_p2 AS spent_p2
+    SELECT sumhh.hhkey, brand, sumhh.spent_p1 * rawhh.continuo AS spent_p1, sumhh.spent_p2 * rawhh.continuo AS spent_p2
     FROM `gfk-science-prod-gold.cp_gain_loss_bachelor_thesis.raw_data_households_2019_2020` rawhh, (
-        SELECT hhkey, marke, SUM(spent_p1) AS spent_p1, SUM(spent_p2) AS spent_p2
+        SELECT hhkey, brand, SUM(spent_p1) AS spent_p1, SUM(spent_p2) AS spent_p2
         FROM table_hh_brand_period
-        GROUP BY hhkey, marke
+        GROUP BY hhkey, brand
     ) sumhh
     WHERE sumhh.hhkey = rawhh.hhkey
     AND rawhh.continuo != 0
@@ -131,7 +131,7 @@ table_hh_brand_sums AS (
 ),
 table_hh_brand_cseg AS (
     -- Calculate CSEG per household, brand and period (table 2.6)
-    SELECT tper.hhkey, marke, spent_p1, spent_p2,
+    SELECT tper.hhkey, brand, spent_p1, spent_p2,
         spent_p1 * IFNULL(SAFE_DIVIDE(cseg, total_spent_p1), 0) AS cseg_p1,
         spent_p2 * IFNULL(SAFE_DIVIDE(cseg, total_spent_p2), 0) AS cseg_p2
     FROM table_hh_period tper
@@ -141,7 +141,7 @@ table_hh_brand_cseg AS (
 table_hh_gl_b2b AS (
     -- Gains and losses from all brands to all other brands, per household (table 2.7)
     -- Self-join for comparing brands to each other (in the same household)
-    SELECT b1.hhkey, b1.marke AS B1, b2.marke AS B2,
+    SELECT b1.hhkey, b1.brand AS B1, b2.brand AS B2,
         b2.cseg_p1 * IFNULL(SAFE_DIVIDE(b1.cseg_p2, cseg), 0) AS hh_gains_b1_to_b2,
         b1.cseg_p1 * IFNULL(SAFE_DIVIDE(b2.cseg_p2, cseg), 0) AS hh_losses_b1_to_b2
     FROM table_hh_brand_cseg b1
@@ -159,4 +159,4 @@ ORDER BY B1, B2;
 
 END;
 
-CALL cp_gain_loss_bachelor_thesis.calc_gain_loss_rp_fmcg_prototype(start_p1, end_p1, start_p2, end_p2);
+CALL cp_gain_loss_bachelor_thesis.calc_gain_loss_rp_fmcg_prototype(d_start_p1, d_end_p1, d_start_p2, d_end_p2);
